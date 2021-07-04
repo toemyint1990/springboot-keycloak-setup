5c1b326365ab6bc7de90d3d18a9fb8ddfb48e2f8
DESKTOP-EDTDSDG\toemy <toemyintthuster@gmail.com>
2021-07-04 20:49:06 +0800

    Initial commit

---
+++ src/main/java/com/project/handyservice/HandyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.project.handyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandyServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/handyservice/HandyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.handyservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
